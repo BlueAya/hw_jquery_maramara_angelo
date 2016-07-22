37f732a4bb22367de26b12a64178a45a0f0955b7
Angelo Maramara <angelo.maramara@gmail.com>
2016-07-22 10:36:35 -0400

    Style all paragraphs with jQuery

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    var paragraphs = $('p');
+
+    paragraphs.css({
+        'color': '#9dc8c8',
+        'font-weight': 'bold',
+        'background-color': '#519d9e'
+    });
+});
